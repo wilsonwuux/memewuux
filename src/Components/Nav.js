8fc6c408500f2ecf1f3dcc76854787c455648aaa
wilsonwuux <71638140+wilsonwuux@users.noreply.github.com>
2021-06-08 17:12:59 -0600

    update nav
    change path nav

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,11 +4,11 @@ import {Link} from "react-router-dom";
 const NavBarComponent=()=>{
 return <Container>
         <Navbar bg="dark" variant="dark">
-            <Link style={{textDecoration:'none',color:'white', marginRight:'8px', marginLeft:'8px'}} to="/" ><h4>HOME</h4></Link>
+            <Link style={{textDecoration:'none',color:'white', marginRight:'8px', marginLeft:'8px'}} to="/memewuux/" ><h4>HOME</h4></Link>
             <Nav >
-                <Link style={{textDecoration:'none',color:'white', marginRight:'8px', marginLeft:'8px'}} to="/oldtable">CARDS Memes </Link>
-                <Link style={{textDecoration:'none',color:'white', marginRight:'8px', marginLeft:'8px'}} to="/bootstable">TABLE Memes</Link>
-                <Link style={{textDecoration:'none',color:'white', marginRight:'8px', marginLeft:'8px'}} to="/newmeme"> NEW Memes </Link>
+                <Link style={{textDecoration:'none',color:'white', marginRight:'8px', marginLeft:'8px'}} to="/memewuux/oldtable">CARDS Memes </Link>
+                <Link style={{textDecoration:'none',color:'white', marginRight:'8px', marginLeft:'8px'}} to="/memewuux/bootstable">TABLE Memes</Link>
+                <Link style={{textDecoration:'none',color:'white', marginRight:'8px', marginLeft:'8px'}} to="/memewuux/newmeme"> NEW Memes </Link>
             </Nav>
         </Navbar>
     </Container>}
